9e088913b795025665f2e30f92f21ac91054fe34
Garv Khurana <gk12345garvkhurana@gmail.com>
2021-05-07 01:32:54 +0530

    created - App.js and searchBox.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import 'tachyons';
 
-ReactDOM.render(<CardList />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
