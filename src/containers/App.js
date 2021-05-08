fc2c85885c2b862c9201737a1c4d61fc2a2b8bdd
Garv Khurana <gk12345garvkhurana@gmail.com>
2021-05-09 02:25:50 +0530

    added error boundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry'
 
 class App extends React.Component {
     constructor() {
@@ -40,7 +41,9 @@ class App extends React.Component {
                     <h1 className = 'f1'>Robo Friends</h1>
                     <SearchBox searchChange = {this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots = {filteredRobots}/>
+                        <ErrorBoundry>
+                            <CardList robots = {filteredRobots}/>
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             )
